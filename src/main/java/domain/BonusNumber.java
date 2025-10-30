feaa0d16c671f8e129569399d8290d345f354b10
ibtSdan <ibtsdan@gmail.com>
2025-10-30 21:15:23 +0900

    refactor: 도메인(BonusNumber) 객체가 자체적으로 유효성을 보장하도록 Validator 로직 이전

--- src/main/java/domain/BonusNumber.java
+++ src/main/java/domain/BonusNumber.java
@@ -1,15 +1,34 @@
 package domain;
 
-import validator.BonusNumberValidator;
+
+import java.util.List;
 
 public class BonusNumber {
     private final Integer bonusNumber;
 
     public BonusNumber(String input, WinningNumbers winningNumbers){
-        BonusNumberValidator.notEmpty(input);
-        BonusNumberValidator.checkLottoNumberRange(input);
+        validateNotEmpty(input);
+        validateLottoNumberRange(input);
         Integer number = Integer.parseInt(input);
-        BonusNumberValidator.checkBonusNotInWinningNumbers(number, winningNumbers.getWinningNumbers());
+        valiateBonusNotInWinningNumbers(number, winningNumbers.getWinningNumbers());
         this.bonusNumber = number;
+    }
+
+    private void validateNotEmpty(String input){
+        if (input == null || input.isBlank()){
+            throw new IllegalArgumentException("[ERROR] 입력값이 비어있습니다.");
+        }
+    }
+
+    private void validateLottoNumberRange(String input){
+        if (!input.matches("[1-9]|[1-3][0-9]|4[0-5]")){
+            throw new IllegalArgumentException("[ERROR] 로또 숫자는 1이상 45이하의 정수여야 합니다.");
+        }
+    }
+
+    private void valiateBonusNotInWinningNumbers(Integer bonusNumber, List<Integer> winningNumbers){
+        if (winningNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자는 당첨 숫자와 중복될 수 없습니다.");
+        }
     }
 }
--- src/test/java/domain/BonusNumberTest.java
+++ src/test/java/domain/BonusNumberTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import validator.BonusNumberValidator;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
