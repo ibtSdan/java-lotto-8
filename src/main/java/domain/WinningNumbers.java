25acff0df7ff9bb0707940ce79a1c9e97600a86e
ibtSdan <ibtsdan@gmail.com>
2025-10-30 20:58:03 +0900

    refactor: 도메인(WinningNumbers) 객체가 자체적으로 유효성을 보장하도록 Validator 로직 이전

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -1,7 +1,5 @@
 package domain;
 
-import validator.WinningNumbersValidator;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,10 +7,30 @@ public class WinningNumbers {
     private final List<Integer> winningNumbers;
 
     public WinningNumbers(String input){
-        WinningNumbersValidator.notEmpty(input);
+        validateNotEmpty(input);
         List<String> tokens = List.of(input.split(","));
-        WinningNumbersValidator.checkSize(tokens);
-        WinningNumbersValidator.checkLottoNumberRange(tokens);
+        validateSize(tokens);
+        validateLottoNumberRange(tokens);
         this.winningNumbers = tokens.stream().map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private void validateNotEmpty(String input){
+        if (input == null || input.isBlank()){
+            throw new IllegalArgumentException("[ERROR] 입력값이 비어있습니다.");
+        }
+    }
+
+    private void validateSize(List<String> tokens){
+        if (tokens.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 6개여야 합니다.");
+        }
+    }
+
+    private void validateLottoNumberRange(List<String> tokens){
+        for (String token : tokens){
+            if (!token.matches("[1-9]|[1-3][0-9]|4[0-5]")){
+                throw new IllegalArgumentException("[ERROR] 당첨 숫자는 1이상 45이하의 정수여야 합니다.");
+            }
+        }
     }
 }
