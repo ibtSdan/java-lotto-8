9dc16c7287d0699afab51ab9d58c676361c541f6
ibtSdan <ibtsdan@gmail.com>
2025-10-30 20:49:26 +0900

    test: WinningNumbersTest 생성

---
+++ src/test/java/domain/WinningNumbersTest.java
@@ -0,0 +1,54 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import validator.WinningNumbersValidator;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class WinningNumbersTest {
+
+    @Nested
+    @DisplayName("성공 입력 테스트")
+    class ValidInputTest{
+
+        @Test
+        void 정상_입력_성공(){
+            assertDoesNotThrow(() -> new WinningNumbers("1,2,3,4,5,6"));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("예외 입력 테스트")
+    class InvalidInputTest{
+
+        @Test
+        void 빈_문자열_입력(){
+            assertThatThrownBy(() -> new WinningNumbers(""))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 길이가_6이_아닌_입력(){
+            assertThatThrownBy(() -> new WinningNumbers("a,b,c"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 숫자가_아닌_입력(){
+            assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,1a"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 범위를_벗어나는_입력(){
+            assertThatThrownBy(() -> new WinningNumbers("1,2,3,47,5,6"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
