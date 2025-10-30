cf0becd9d778a3fd4c761169d2c584677dde897c
ibtSdan <ibtsdan@gmail.com>
2025-10-30 20:38:57 +0900

    refactor: 도메인(PurchaseAmount) 객체가 자체적으로 유효성을 보장하도록 Validator 로직 이전

--- src/main/java/domain/PurchaseAmount.java
+++ src/main/java/domain/PurchaseAmount.java
@@ -1,16 +1,40 @@
 package domain;
 
-import validator.PurchaseAmountValidator;
 
 public class PurchaseAmount {
     private final int value;
 
     public PurchaseAmount(String input){
-        PurchaseAmountValidator.notEmpty(input);
-        PurchaseAmountValidator.positive(input);
-        PurchaseAmountValidator.checkIntegerRange(input);
+        validateNotEmpty(input);
+        validatePositive(input);
+        validateIntegerRange(input);
         int money = Integer.parseInt(input);
-        PurchaseAmountValidator.multipleOfThousand(money);
+        validateMultipleOfThousand(money);
         this.value = money;
+    }
+
+    private void validateNotEmpty(String input){
+        if (input == null || input.isBlank()){
+            throw new IllegalArgumentException("[ERROR] 입력값이 비어있습니다.");
+        }
+    }
+
+    private void validatePositive(String input){
+        if (!input.matches("[1-9]\\d*")){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 양수여야 합니다.");
+        }
+    }
+
+    private void validateIntegerRange(String input){
+        String maxInt = "2147483647";
+        if (input.compareTo(maxInt) > 0){
+            throw new IllegalArgumentException("[ERROR] 입력값이 너무 큽니다.");
+        }
+    }
+
+    private void validateMultipleOfThousand(Integer money){
+        if (money % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위어야 합니다.");
+        }
     }
 }
